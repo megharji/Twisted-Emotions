eb5366343db447694cab092295c30a421a893f33
megharji <meghareji25@gmail.com>
2024-05-12 23:06:26 +0530

    mongodb url change

--- models/config.js
+++ models/config.js
@@ -1,4 +1,4 @@
 const mongoose = require ("mongoose")
-mongoose.connect("mongodb://127.0.0.1:27017/Twisted")
+mongoose.connect(process.env.MONGODB_URL)
         .then(()=>console.log("DB Connected"))
         .catch((err)=> console.log(err.message))
